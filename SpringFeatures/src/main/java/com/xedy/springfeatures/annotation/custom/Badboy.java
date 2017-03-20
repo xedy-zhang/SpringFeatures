bbd333506f3a890badcbf74f8498d83e39cf17c1
xedy.zhang <xedy_zhang@126.com>
2017-03-21 00:07:54 +0800

    添加spring-context。拟用WebApplicationInitializer解析自定义注解。

--- SpringFeatures/src/main/java/com/xedy/springfeatures/annotation/custom/Badboy.java
+++ SpringFeatures/src/main/java/com/xedy/springfeatures/annotation/custom/Badboy.java
@@ -6,14 +6,13 @@ package com.xedy.springfeatures.annotation.custom;
 import static java.lang.annotation.ElementType.FIELD;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Target(FIELD)
-/**
- * @author xedy_zhang@126.com
- *
- */
 public @interface Badboy {
-	public String value="";
+	public String value() default "";
 }
